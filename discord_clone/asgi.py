e146f2c1fcf25ef9858090ff5a222c12b817beea
pawel-pawel <pawelitwojcik@gmail.com>
2026-05-02 15:59:00 +0200

    asg i setttings

--- discord_clone/asgi.py
+++ discord_clone/asgi.py
@@ -1,4 +1,8 @@
 import os
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'discord_clone.settings')
 
--- discord_clone/settings.py
+++ discord_clone/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9#kkbnglhgj6-fp^9b%e^gkc)44hbd(%x-_seo*#^dwde2z459'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
